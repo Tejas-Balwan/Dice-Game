edb234dd44fa3ec6602687a24800e520e6f328d7
Tejas-Balwan <108165964+Tejas-Balwan@users.noreply.github.com>
2022-09-10 12:52:43 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// Logic for random image - 1
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+document.querySelector(".img1").setAttribute("src","images/dice" + randomNumber1 + ".png");
+
+// Logic for random image - 2
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+document.querySelector(".img2").setAttribute("src","images/dice" + randomNumber2 + ".png");
+
+// Logic for winning messages
+if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
